36e513506ad7c881a206904564b1ff87dfae1b57
shannon-nabors <shannon.nabors@gmail.com>
2019-02-08 15:35:43 -0500

    added basic routes and views for user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,46 @@
 class UsersController < ApplicationController
+
+  before_action :get_user, only: [:show, :edit, :update]
+
+  def show
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    user = User.new(user_params)
+    if user.save
+      redirect_to user_path(user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    User.destroy(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :username)
+  end
+
+  def get_user
+    @user = User.find(params[:id])
+  end
+
 end
