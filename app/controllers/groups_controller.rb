a60b7ea2416223a2fa51afe3ddc2ea8d52d39467
github username <metelskyi88@gmail.com>
2019-02-12 13:13:13 -0500

    User can leave/join a group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -6,8 +6,21 @@ class GroupsController < ApplicationController
     @group = Group.create(group_params)
     redirect_to @group
   end
+
   def show
     @group = Group.find(params[:id])
+  end
+
+  def leave
+    @group = Group.find(params[:id])
+    @group.users.delete(current_user.id)
+    redirect_to @group
+  end
+
+  def join
+    @group = Group.find(params[:id])
+    @group.users << current_user
+    redirect_to @group
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :groups, only: [:show, :create, :new]
   resources :insults, only: [:new, :create, :show, :delete]
   resources :users, except: [:index]
+  post '/leave/:id', to: 'groups#leave', as: 'leave'
+  post "/join/:id", to: 'groups#join', as: 'join'
   get '/home', to: 'users#home'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
