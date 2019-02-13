9e471edf2efe2c42d3e120d59ffff17a55abefa3
shannon-nabors <shannon.nabors@gmail.com>
2019-02-13 14:28:01 -0500

    added bootstrap

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,19 @@
 class ApplicationController < ActionController::Base
   helper_method :current_user
+  helper_method :current_user_groups
+  helper_method :current_user_insults
   helper_method :logged_in?
 
   def current_user
     User.find_by(id: session["user_id"])
+  end
+
+  def current_user_groups
+    current_user.groups
+  end
+
+  def current_user_insults
+    current_user_groups.map(&:insults).flatten
   end
 
   def logged_in?
