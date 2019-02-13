d5cc80dee859bcb7d530e29ff5ba9b20759b9a5c
github username <metelskyi88@gmail.com>
2019-02-13 09:22:19 -0500

    fixed a login bug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,7 +6,7 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(username: params[:username])
-    if @user.authenticate(params[:password])
+    if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       redirect_to home_path
     else
