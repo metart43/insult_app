8be5590569b9da3e36ec71e5d34e52ed84188fef
shannon-nabors <shannon.nabors@gmail.com>
2019-02-14 18:12:26 -0500

    changed top 5 list to reflect just that group

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,7 +2,7 @@ class Group < ApplicationRecord
   has_many :user_groups
   has_many :users, through: :user_groups
   has_many :insults
-  
+
   validates :name, presence: true, uniqueness: true, length: { in: 6..20 }
 
   def self.search(search)
@@ -13,8 +13,12 @@ class Group < ApplicationRecord
     end
   end
 
+  def count_users_insults_in_group(user)
+    user.insults.where(group_id: self.id).count
+  end
+
   def top_5
-    self.users.sort_by {|u| u.insults.count}.reverse.first(5)
+    self.users.sort_by {|u| count_users_insults_in_group(u)}.reverse.first(5)
   end
 
 end
